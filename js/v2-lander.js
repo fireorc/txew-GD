0166ed16e794e1e72fb0951c9004adbda01f61bd
fireorc <fireorc@users.noreply.github.com>
2021-04-28 08:40:53 -0400

    Add files via upload

--- js/v2-lander.js
+++ js/v2-lander.js
@@ -1,5 +1,5 @@
-document.getElementById('cont2').innerHTML = "names@genuine.domains";
-
 /*
-document.getElementById('cont2').innerHTML = "names@genuine.domains<br/><span style='color: gray; font-family: GenuineDomains;'>(use only '</span><span style='color: red; font-family: GenuineDomains;'>domain name inquiry</span><!--<span style='color: gray; font-family: GenuineDomains;'>' sans quotes in email subject line)</span>-->";
+document.getElementById('cont2').innerHTML = "names@genuine.domains";
 */
+
+document.getElementById('cont2').innerHTML = "names@genuine.domains<br/><span style='color: gray; font-family: GenuineDomains;'>(use only '</span><span style='color: red; font-family: GenuineDomains;'>domain name inquiry</span><span style='color: gray; font-family: GenuineDomains;'>' sans quotes in email subject line)</span>";
