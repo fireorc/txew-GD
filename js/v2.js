4961550233869cd672ccca722e435a7fdc578aa4
fireorc <fireorc@users.noreply.github.com>
2021-05-01 18:32:19 -0400

    Add files via upload

--- js/v2.js
+++ js/v2.js
@@ -1,3 +1,4 @@
+document.getElementById('inquiry').innerHTML = "<span style='color: rgba(153,153,153,1);'>(use '</span>domain inquiry<span style='color: rgba(153,153,153,1);'>' in email subject line)</span></span>";
 document.getElementById('footer-content').innerHTML = "<span style='font-size: 18px;'>© Genuine Domains, est. 2016</span>";
 /* in conjunction with css contact and form-deco id
 function contact() {
