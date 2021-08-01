0a97f514aa550eaa4ecd6e45f42ea159ea7b1ab3
fireorc <fireorc@users.noreply.github.com>
2021-08-01 09:23:28 -0400

    Add files via upload

--- js/lander.js
+++ js/lander.js
@@ -1,3 +1,3 @@
-document.getElementById('footmain').innerHTML = "✉ names@genuine.domains<br/><span style='color: rgba(153,153,153,1);'>(use '</span>domain inquiry<span style='color: rgba(153,153,153,1);'>' in email subject line)</span></span>";
+document.getElementById('footmain').innerHTML = "<span style='color: rgba(153,153,153,1);font-style: italic;'>Please fill out the form above or send an email to contact the domain name owner.</span><br/><br/>✉ names@genuine.domains<br/><span style='color: rgba(153,153,153,1);'>(use '</span>domain inquiry<span style='color: rgba(153,153,153,1);'>' in email subject line)</span></span>";
 
 document.getElementById('footinfo').innerHTML = "&copy; Genuine Domains • est. 2016";
