5180caceec69d0b337f758c94fbfa49a9612bb85
fireorc <fireorc@users.noreply.github.com>
2021-10-23 13:06:13 -0400

    Add files via upload

--- js/style.js
+++ js/style.js
@@ -5,4 +5,6 @@ document.getElementById('extralink2').innerHTML = "<a href='disclaimer.html'>dis
 
 document.getElementById('footmain').innerHTML = "☎ 1-866-GENDOMS<br/>✉ names@genuine.domains<br/><span style='color: rgba(153,153,153,1);'>(use '</span>domain inquiry<span style='color: rgba(153,153,153,1);'>' in email subject line)</span></span>";
 
-document.getElementById('footinfo').innerHTML = "&copy; Genuine Domains • est. 2016 • <span id='cryptopay' title='cryptopay: gendoms.crypto'>₿</span> • <a href='http://dn.gd'>dn.gd</a> • <a href='https://racket.com/genuine'><img src='images/racket.svg'></a>";
+/*document.getElementById('footinfo').innerHTML = "&copy; Genuine Domains • est. 2016 • <span id='cryptopay' title='crypto payments: gendoms.crypto'>₿</span> • <a href='http://dn.gd'>dn.gd</a> • <a href='https://racket.com/genuine'><img src='images/racket.svg'></a>";*/
+
+document.getElementById('footinfo').innerHTML = "&copy; Genuine Domains • est. 2016 • <span id='cryptopay' title='crypto payments: gendoms.crypto'>₿</span> • <a href='http://dn.gd'>dn.gd</a>";
