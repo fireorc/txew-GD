0d654e92039cae1628a1415931405a674ddc6944
fireorc <fireorc@users.noreply.github.com>
2021-11-28 18:01:41 -0500

    Add files via upload

---
+++ style.js
@@ -0,0 +1,12 @@
+/*document.getElementById('dnlink1').innerHTML = "<a href='classic.html'>classic</a>";
+document.getElementById('dnlink2').innerHTML = "<a href='gTLD.html'>gTLD</a>";
+document.getElementById('extralink1').innerHTML = "<a href='spotlight.html'>spotlight</a>";
+document.getElementById('extralink2').innerHTML = "<a href='disclaimer.html'>disclaimer</a>";*/
+
+document.getElementById('extralink1').innerHTML = "<a href='handshake.html'>handshake</a>";
+
+document.getElementById('footmain').innerHTML = "☎ 1-866-GENDOMS<br/>✉ names@genuine.domains<br/><span style='color: rgba(153,153,153,1);'>(use '</span>domain inquiry<span style='color: rgba(153,153,153,1);'>' in email subject line)</span></span>";
+
+/*document.getElementById('footinfo').innerHTML = "&copy; Genuine Domains • est. 2016 • <span id='cryptopay' title='crypto payments: gendoms.crypto'>₿</span> • <a href='http://dn.gd'>dn.gd</a> • <a href='https://racket.com/genuine'><img src='images/racket.svg'></a>";*/
+
+document.getElementById('footinfo').innerHTML = "&copy; Genuine Domains • est. 2016 • <span id='cryptopay' title='crypto payments: gendoms.crypto'>₿</span> • <a href='http://dn.gd'>dn.gd</a>";
