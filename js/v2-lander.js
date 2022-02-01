e69b3dd53d038ba36f6b9c9ee9e0c17706d92332
fireorc <fireorc@users.noreply.github.com>
2022-01-31 19:03:26 -0500

    Delete v2-lander.js

--- js/v2-lander.js
+++
@@ -1,5 +0,0 @@
-/*
-document.getElementById('cont2').innerHTML = "names@genuine.domains";
-*/
-
-document.getElementById('cont2').innerHTML = "names@genuine.domains<br/><span style='color: gray; font-family: GenuineDomains;'>(use '</span><span style='color: red; font-family: GenuineDomains;'>domain inquiry</span><span style='color: gray; font-family: GenuineDomains;'>' in email subject line)</span>";
